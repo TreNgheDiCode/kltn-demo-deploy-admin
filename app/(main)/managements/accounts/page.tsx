8fe262eb10dcbc38dbe9c0d51d6e6e63c9025d32
TreNgheDiCode <gabayan170@gmail.com>
2024-05-06 19:28:02 +0700

    Complete Accounts Table

--- app/(main)/managements/accounts/page.tsx
+++ app/(main)/managements/accounts/page.tsx
@@ -1,5 +1,20 @@
-const AccountsPage = () => {
-  return <div>Accounts</div>;
+import { AccountsTable } from "@/components/accounts/accounts-table";
+import { HeadingAccount } from "@/components/accounts/heading-account";
+import { GetAccountLib } from "@/lib/account";
+
+const AccountsPage = async () => {
+  const accounts = await GetAccountLib();
+
+  if (!accounts || !accounts.every((account) => account.student)) {
+    return <div>Không có dữ liệu</div>;
+  }
+
+  return (
+    <div className="space-y-4">
+      <HeadingAccount />
+      <AccountsTable accounts={accounts} />
+    </div>
+  );
 };
 
 export default AccountsPage;
