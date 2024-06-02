9f16cfad8746359606208087c439ee08948663c8
Huongcutee <nguyensinhhuong123@gmail.com>
2024-06-03 03:14:07 +0700

    code lai do mer nen mat code

--- app/api/accounts/students/profiles/[studentCode]/posts/[postId]/route.ts
+++ app/api/accounts/students/profiles/[studentCode]/posts/[postId]/route.ts
@@ -1,43 +1,54 @@
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
-export async function GET(
+export async function DELETE(
   req: Request,
   { params }: { params: { postId: string } }
 ) {
   try {
     if (!params.postId) {
       return NextResponse.json(
-        { error: "Vui lòng truyền vào mã bài đăng" },
+        { error: "Không tìm thấy post ID" },
         { status: 400 }
       );
     }
 
-    const post = await db.post.findUnique({
+    const postSave = await db.postSave.findFirst({
+      where: {
+        postId: params.postId,
+      },
+    });
+    if (postSave) {
+      await db.postSave.delete({
+        where: {
+          id: postSave.id,
+        },
+      });
+    }
+    const postLike = await db.postLike.findFirst({
+      where: {
+        postId: params.postId,
+      },
+    });
+    if (postLike) {
+      await db.postLike.delete({
+        where: {
+          id: postLike.id,
+        },
+      });
+    }
+
+    const post = await db.post.delete({
       where: {
         id: params.postId,
       },
-      include: {
-        comments: {
-          include: {
-            likes: true,
-            children: true,
-          },
-        },
-        images: true,
-        likes: true,
-        saves: true,
-        shares: true,
-      },
     });
+    if (!post) {
+      return Response.json({ error: "Lỗi xóa bài viết" }, { status: 404 });
+    }
 
-    return NextResponse.json(post, { status: 200 });
+    return NextResponse.json({ success: "Xóa thành công" }, { status: 200 });
   } catch (error) {
-    console.log("ERROR GET POST ID FULL", error);
-
-    return NextResponse.json(
-      { error: "Lỗi lấy thông tin đầy đủ bài đăng" },
-      { status: 500 }
-    );
+    return NextResponse.json({ error: "Lỗi " }, { status: 404 });
   }
 }
